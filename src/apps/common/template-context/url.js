744f36286a2faab00f5e3311b992ea479b520c02
Viacheslav Bereza <betula.icons8@gmail.com>
2015-07-07 13:54:21 +0300

    fix resources url

--- src/apps/common/template-context/url.js
+++ src/apps/common/template-context/url.js
@@ -3,6 +3,10 @@ var path = require('path');
 
 var createUrl = function(base, url) {
   url = (url || '').trim();
+  if (!base) {
+    return url;
+  }
+
   if (url.charAt(0) == '/') {
     url = url.substr(1);
   }
