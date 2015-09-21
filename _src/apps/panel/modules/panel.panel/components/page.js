0993b4a073e4afefd87150794a01a1f2efd6228c
Alexey Zakharenko <yogin.alex@gmail.com>
2015-09-21 13:46:04 +0300

    Little redesign

--- _src/apps/panel/modules/panel.panel/components/page.js
+++ _src/apps/panel/modules/panel.panel/components/page.js
@@ -1,8 +1,11 @@
 
-ng.directive('page', [
-  function() {
+ng.directive('page', ['config',
+  function(config) {
     return {
       restrict: 'E',
+      link: function(scope) {
+        scope.config = config;
+      },
       templateUrl: 'components/page.html'
     }
   }
